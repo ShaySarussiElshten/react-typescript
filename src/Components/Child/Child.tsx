79430620ce92c3dc96ec174cb115d32989e671aa
Shay Sarussi <ssarussi@corp.devalore.com>
2022-04-25 21:40:16 +0300

    first commit

---
+++ src/Components/Child/Child.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Styled from './Chiled.style'
+
+
+const Child = () => {
+
+  return (
+    <>
+        <Styled.Header>HELLO</Styled.Header>
+        <Styled.SubmitButton type="submit">push the button</Styled.SubmitButton>
+       
+    </>
+  )
+}
+
+
+export default Child
---
+++ src/Components/Child/Chiled.style.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+
+
+const Header = styled.h1`
+   font-size:22px;
+   color:red;
+`;
+
+const SubmitButton = styled.button`
+        background-color: #4CAF50; 
+        border: none;
+        color: white;
+        padding: 15px 32px;
+        text-align: center;
+        text-decoration: none;
+        font-size: 16px;
+`;
+
+const Styled ={
+    Header,
+    SubmitButton
+}
+
+export default Styled;
