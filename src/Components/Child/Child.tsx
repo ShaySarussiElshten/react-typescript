84697b8a79d3a1e6d444e891ce75830fb2d50d6a
Shay Sarussi <ssarussi@corp.devalore.com>
2022-04-25 22:53:18 +0300

    add tw styled component

--- src/Components/Child/Child.tsx
+++ src/Components/Child/Child.tsx
@@ -6,8 +6,22 @@ const Child = () => {
 
   return (
     <>
-        <Styled.Header>HELLO</Styled.Header>
-        <Styled.SubmitButton type="submit">push the button</Styled.SubmitButton>
+        <Styled.Continer>
+        <Styled.WarapperContainer>
+          <Styled.BigHeader>
+            <span className="block">Ready to dive in?</span>
+            <span className="block">Start your free trial today.</span>
+          </Styled.BigHeader>
+          <div className="mt-8 flex justify-center">
+            <div className="inline-flex rounded-md shadow">
+              <a href="#" className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"> Get started </a>
+            </div>
+            <div className="ml-3 inline-flex">
+              <a href="#" className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200"> Learn more </a>
+            </div>
+          </div>
+        </Styled.WarapperContainer>
+      </Styled.Continer>
        
     </>
   )
--- src/Components/Child/Chiled.style.ts
+++ src/Components/Child/Chiled.style.ts
@@ -1,25 +1,39 @@
 import styled from 'styled-components';
+import tw from 'tailwind-styled-components';
 
 
+// const Header = styled.h1`
+//    font-size:22px;
+//    color:red;
+// `;
 
-const Header = styled.h1`
-   font-size:22px;
-   color:red;
+const Continer = tw.div`
+   bg-white
 `;
 
-const SubmitButton = styled.button`
-        background-color: #4CAF50; 
-        border: none;
-        color: white;
-        padding: 15px 32px;
-        text-align: center;
-        text-decoration: none;
-        font-size: 16px;
+const WarapperContainer = tw.div`
+   max-w-7xl 
+   mx-auto 
+   text-center 
+   py-12 
+   px-4 
+   sm:px-6 
+   lg:py-16 
+   lg:px-8
 `;
+
+const BigHeader = tw.h2`
+  text-3xl 
+  font-extrabold 
+  tracking-tight 
+  text-gray-900 
+  sm:text-4xl
+`
 
 const Styled ={
-    Header,
-    SubmitButton
+    Continer,
+    WarapperContainer,
+    BigHeader
 }
 
 export default Styled;
