1f66bcef2b0262afceeb87af478388ba7d4e9352
Shay Sarussi <ssarussi@corp.devalore.com>
2022-05-02 20:08:54 +0300

    tailwind-and-styled-component

--- src/Components/Child/Child.tsx
+++ src/Components/Child/Child.tsx
@@ -4,22 +4,26 @@ import Styled from './Chiled.style'
 
 const Child = () => {
 
+  
+
+  const x = false
+
   return (
     <>
-        <Styled.Continer>
+      <Styled.Continer>
         <Styled.WarapperContainer>
-          <Styled.BigHeader>
-            <span className="block">Ready to dive in?</span>
-            <span className="block">Start your free trial today.</span>
-          </Styled.BigHeader>
-          <div className="mt-8 flex justify-center">
-            <div className="inline-flex rounded-md shadow">
-              <a href="#" className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"> Get started </a>
-            </div>
-            <div className="ml-3 inline-flex">
-              <a href="#" className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200"> Learn more </a>
-            </div>
-          </div>
+          <Styled.BigHeaderWarrapper>
+            <Styled.HeaderText>Ready to dive in?</Styled.HeaderText>
+            <Styled.HeaderText>Start your free trial today.</Styled.HeaderText>
+          </Styled.BigHeaderWarrapper>
+          <Styled.ButtunsContainer>
+            <Styled.ButtunWarrapperOne>
+               <Styled.ButtonOne herf="#" >Get started</Styled.ButtonOne>
+            </Styled.ButtunWarrapperOne>
+            <Styled.ButtunWarrapperTwo>
+              <Styled.ButtonTwo herf="#" isMarginLeft={x} >Learn more</Styled.ButtonTwo>
+            </Styled.ButtunWarrapperTwo>
+          </Styled.ButtunsContainer>
         </Styled.WarapperContainer>
       </Styled.Continer>
        
